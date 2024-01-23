e50a7f9679d237998c1d45f3e8d3ab04db390d73
btmnk <contact@bitmonk.de>
2024-01-23 22:50:57 +0100

    fixes entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-app_id=2278520
-install_dir="/home/steam/enshrouded"
-
 echo " --- Updating Enshrouded Server..."
-steamcmd +force_install_dir $install_dir +login anonymous +app_update $app_id validate +quit
+./steamcmd +@sSteamCmdForcePlatformType windows +force_install_dir /home/steam/enshrouded +login anonymous +app_update 2278520 +quit
 echo " --- Update Done"
 
-su steam -c "xvfb-run --auto-servernum wine64 /home/steam/enshrouded/enshrouded_server.exe"
-echo "Server started."
+xvfb-run --auto-servernum wine64 /home/steam/enshrouded/enshrouded_server.exe
+echo " --- Server started."
 /bin/bash
